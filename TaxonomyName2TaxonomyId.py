66405aefc0cf11bf0e404e5080595b0ef925376a
harazono <6312081@ed.tus.ac.jp>
2020-05-12 16:30:44 +0900

    WIP

--- TaxonomyName2TaxonomyId.py
+++ TaxonomyName2TaxonomyId.py
@@ -4,34 +4,34 @@ import pprint
 import argparse
 import requests
 import json
+import openpyxl
 import xml.etree.ElementTree as ET
 pp = pprint.PrettyPrinter(indent=4)
 
+def TaxonomyName2TaxonomyId(name):
+	name4url = name.strip().replace(' ', "%20")
+	url      = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.cgi?db=taxonomy&term=%s"%(name4url)
+	try:
+		res  = requests.get(url).text
+		root = ET.fromstring(res)
+		taxonomy_id = root.find("IdList")[0].text
+		return "%s&%s"%(name.strip(), taxonomy_id)
+	except:
+		return sys.exc_info()
+
+#def manageExcelFile:
 
 def main():
 	parser = argparse.ArgumentParser(description = 'transform from taxonomy name to taxonomy id')
 	parser.add_argument("filename", help = "a plane txt file which contains taxonomy name in each line")
+	#parser.add_argument("", help = "a plane txt file which contains taxonomy name in each line")
 	args = parser.parse_args()
 	filename = args.filename
 	name_id_pair_array = [] * 20
 	with open(filename) as f:
 		for line in f:
-			taxonomy_name = line.strip().replace(' ', "%20")
-			url  = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.cgi?db=taxonomy&term=%s"%(taxonomy_name)
-			try:
-				res  = requests.get(url).text
-				root = ET.fromstring(res)
-				taxonomy_id   = root.find("IdList")[0].text
-				taxonomy_name = line
-				print("%s&%s||"%(taxonomy_name.strip(), taxonomy_id))
-			except:
-				print(sys.exc_info())
-
-
-		print()
-
+			name_id_pair_array.append(TaxonomyName2TaxonomyId(line))
+	print("||\n".join(name_id_pair_array))
 
 if __name__ == '__main__':
 	main()
-
-	#https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.cgi?db=taxonomy&term=Homo%20sapiens+OR+Mus%20musculus
